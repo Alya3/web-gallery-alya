259dcbfabd086ba85ffc5cd0bdb20148427d6654
ai kartika <aykrtkaslw@gmail.com>
2024-02-26 17:08:24 +0700

    ukk bonet

---
+++ resources/views/admin/partials/_footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="footer text-center text-muted">
+    All Rights Reserved by SMK Al Hafidz Leuwiliang. Designed and Developed by <a href="https://instagram.com/alxya_30" target="_blank">Alya Junia Rahmawati</a>.
+</footer>
